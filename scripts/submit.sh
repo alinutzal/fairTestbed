bf9bd8ce4dd57ca72c05b62e2591ccb390f6684b
Alina <alinutzal@gmail.com>
2023-09-18 09:45:37 -0400

    slurm update

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --gpus-per-node=2
 #SBATCH --account=pls0144
 #SBATCH --time=05:00:00
@@ -12,6 +13,14 @@
 #SBATCH --mail-user=alazar@ysu.edu
 
 export NCCL_NET_GDR_LEVEL=PHB
+export NCCL_P2P_LEVEL=NVL
+
+##### Number of total processes 
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+echo "Nodelist:= " $SLURM_JOB_NODELIST
+echo "Number of nodes:= " $SLURM_JOB_NUM_NODES
+echo "Ntasks per node:= "  $SLURM_NTASKS_PER_NODE
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
 
 module load cuda/11.8.0
 source activate torch
